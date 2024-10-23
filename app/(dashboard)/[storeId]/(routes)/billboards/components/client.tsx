e2a960e2d46e542f00d1bacfc22837e67daa4c4e
catmaker <rzt0317@nate.com>
2024-10-23 18:00:35 +0900

    feat: Add Billboard page and client component

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import React, { useEffect } from "react";
+import { useRouter, useParams } from "next/navigation";
+import Heading from "@/components/ui/heading";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
+const BillboardClient = () => {
+  const router = useRouter();
+  const params = useParams();
+  console.log(params);
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title="Billboards (0)"
+          description="Manage billboards for your store"
+        />
+        <Button
+          className="flex items-center gap-x-2"
+          size="sm"
+          onClick={() => router.push(`/${params.storeId}/billboards/new`)}
+        >
+          <Plus className="mr-2 h-4 w-4" />
+          Add New
+        </Button>
+      </div>
+      <Separator />
+    </>
+  );
+};
+
+export default BillboardClient;
