f95cbaa57f99d8861adf8cd344776bc714ad58ea
catmaker <rzt0317@nate.com>
2024-10-30 17:59:21 +0900

    feat: Implement homepage layout with billboard and featured products

--- shopnroll-store/app/(routes)/page.tsx
+++ shopnroll-store/app/(routes)/page.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import Container from "@/components/ui/container";
+import Billboard from "@/components/billboard";
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import ProductList from "@/components/product-list";
 
-const HomePage = () => {
-  return <div>HomePage</div>;
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard("0fc37012-203f-4f1c-9ce0-4944e0c7e83a");
+  const products = await getProducts({
+    categoryId: "0fc37012-203f-4f1c-9ce0-4944e0c7e83a",
+    isFeatured: true,
+  });
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+        <ProductList title="Featured Products" items={products} />
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
