a816a1794c56b574fcd8c4c14d532ee0968a2f30
catmaker <rzt0317@nate.com>
2024-10-31 20:24:54 +0900

    feat: Create Info component for product details

--- shopnroll-store/app/(routes)/product/[productId]/page.tsx
+++ shopnroll-store/app/(routes)/product/[productId]/page.tsx
@@ -3,6 +3,7 @@ import getProduct from "@/actions/get-product";
 import Container from "@/components/ui/container";
 import ProductList from "@/components/product-list";
 import Gallery from "@/components/gallery";
+import Info from "@/components/info";
 
 interface ProductPageProps {
   params: {
@@ -15,14 +16,13 @@ const ProductPage = async ({ params }: ProductPageProps) => {
   const relatedProducts = await getProducts({
     categoryId: product?.category?.id,
   });
-  console.log(relatedProducts);
   return (
     <div className="bg-white">
       <Container>
         <div className="px-4 py-10 sm:px-6 lg:px-8">
           <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
             <Gallery images={product?.images} />
-            <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">Info</div>
+            <Info product={product} />
           </div>
           <hr className="my-10" />
           <ProductList
--- shopnroll-store/components/info.tsx
+++ shopnroll-store/components/info.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from "react";
+import { Product } from "@/lib/types";
+import Currency from "@/components/ui/currency";
 
-const Info = () => {
-  return <div>Info</div>;
+interface InfoProps {
+  product: Product;
 }
+
+const Info = ({ product }: InfoProps) => {
+  return (
+    <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
+      <h1 className="text-xl font-bold text-gray-900">{product.name}</h1>
+      <div className="mt-4 flex items-end justify-between">
+        <p className="text-2xl font-bold text-gray-900">
+          <Currency value={product.price} />
+        </p>
+      </div>
+      <div className="mt-4 flex items-end justify-between">
+        <p className="text-sm text-gray-500">{product.description}</p>
+      </div>
+    </div>
+  );
+};
 
 export default Info;
