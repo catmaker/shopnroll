ad8b21aadba7c5dd4139fd12fed358df0b050a67
신지수 <114233139+catmaker@users.noreply.github.com>
2024-11-05 21:56:41 +0900

    Update middleware.ts

--- shopnroll-admin/middleware.ts
+++ shopnroll-admin/middleware.ts
@@ -1,13 +1,13 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware } from '@clerk/nextjs/server'
 
-// Force Edge Runtime
-export const runtime = "edge";
+// Use experimental-edge instead of edge
+export const runtime = 'experimental-edge';
 
 export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/(api|trpc)(.*)",
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)',
   ],
 };
