6a375dbb47526597e0db144898df41907f1fbb1d
신지수 <114233139+catmaker@users.noreply.github.com>
2024-11-05 22:28:22 +0900

    Update middleware.ts

--- shopnroll-admin/middleware.ts
+++ shopnroll-admin/middleware.ts
@@ -1,13 +1,10 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+import { authMiddleware } from "@clerk/nextjs/server";
 
-// Use experimental-edge instead of edge
-export const runtime = 'experimental-edge';
-
-export default clerkMiddleware();
+// 모든 라우트를 public으로 설정
+export default authMiddleware({
+  publicRoutes: ["/(.*)", "/api(.*)"], 
+});
 
 export const config = {
-  matcher: [
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    '/(api|trpc)(.*)',
-  ],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
